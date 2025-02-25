f3ffb49a97a59882c7c7a77aa70147c34909adee
martinolanza03 <lanzamartino3@gmail.com>
2025-02-25 15:33:29 +0100

    feat: add comments and fix synstax

--- Js/app.js
+++ Js/app.js
@@ -1,12 +1,14 @@
+// ciclo per scrivere i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log ('FizzBuzz')
-    } else if (i % 3 == 0 ) {
-        console.log ('Fizz')
-    } else if (i % 5 == 0 ) {
-        console.log ('Buzz')
-    } else {
-        console.log (i)
+    
+    if ((i % 3 == 0) && (i % 5 == 0)) { // controllo inizialmente se il numero è multiplo di 3 o 5
+        console.log ('FizzBuzz');
+    } else if (i % 3 == 0 ) { // controllo se il numero è multiplo di 3
+        console.log ('Fizz');
+    } else if (i % 5 == 0 ) { // controllo se il numero è multiplo di 5
+        console.log ('Buzz');
+    } else { // se non è multiplo di 5 o 3, stampo il numero
+        console.log (i);
     }
 }
